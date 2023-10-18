57ab8c25a544d1be9e6a97ee191fc8b84c2b64dd
noesmc <nikita_sinykovich@mail.ru>
2023-10-18 21:48:37 +0300

    Ja_2_2

--- src/Main.java
+++ src/Main.java
@@ -13,3 +13,22 @@ public class Main {
         System.out.println("The sum of all numbers from 0 to " + x + " is " + ((x >= 0) ? sum : -sum));
     }
 }
+//        With input check
+//        Scanner scanner = new Scanner(System.in);
+//        int x;
+//        int sum = 0;
+//        while (true) {
+//            System.out.print("Please enter a number: ");
+//            try {
+//                x = Integer.parseInt(scanner.next());
+//                for (int i = 0; i <= abs(x); i++) {
+//                    sum += i;
+//                }
+//                System.out.println("The sum of all numbers from 0 to " + x + " is " + ((x >= 0) ? sum : -sum));
+//                break;
+//            } catch (NumberFormatException e) {
+//                System.out.println("Wrong input!");
+//            }
+//        }
+//    }
+//}
