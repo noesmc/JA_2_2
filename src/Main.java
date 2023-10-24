1476b710d589ef0e4c988c3a8c742c55ef1ed397
noesmc <nikita_sinykovich@mail.ru>
2023-10-24 06:59:36 +0300

    Ja_2_2

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,23 @@ import static java.lang.Math.abs;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter a number: ");
-        int x = scanner.nextInt();
+        int x;
         int sum = 0;
-        for (int i = 0; i <= abs(x); i++) {
-            sum += i;
+        while (true) {
+            System.out.print("Please enter a number: ");
+            try {
+                x = Integer.parseInt(scanner.next());
+                for (int i = 0; i <= abs(x); i++) {
+                    sum += i;
+                }
+                System.out.printf("The sum of all numbers from 0 to %d is %d",
+                        x, ((x >= 0) ? sum : -sum));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong input!");
+            }
         }
-        System.out.println("The sum of all numbers from 0 to " + x + " is " + ((x >= 0) ? sum : -sum));
     }
 }
-//        With input check
-//        Scanner scanner = new Scanner(System.in);
-//        int x;
-//        int sum = 0;
-//        while (true) {
-//            System.out.print("Please enter a number: ");
-//            try {
-//                x = Integer.parseInt(scanner.next());
-//                for (int i = 0; i <= abs(x); i++) {
-//                    sum += i;
-//                }
-//                System.out.println("The sum of all numbers from 0 to " + x + " is " + ((x >= 0) ? sum : -sum));
-//                break;
-//            } catch (NumberFormatException e) {
-//                System.out.println("Wrong input!");
-//            }
-//        }
-//    }
-//}
